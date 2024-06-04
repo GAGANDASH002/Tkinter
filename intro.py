75b8cc6f57a978e7c30d02c36c66f25e60f09864
GAGANDASH002 <gagandash002@gmail.com>
2024-06-04 21:19:44 +0530

    tkinter structure

---
+++ intro.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+root = Tk()#creates a Basic GUI with its components(creates an object of Tk class)
+
+#GUI logic here
+
+root.mainloop()#all logic of GUI is implemented from here
